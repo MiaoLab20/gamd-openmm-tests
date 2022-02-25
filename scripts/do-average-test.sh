1e27d3336b82f5a3d71fdde9227eb50edf9dfe3e
Matthew Copeland <matthew@ku.edu>
2022-02-25 09:44:01 -0600

    * Cleaned up for handling the directory structure for output analysis.

--- scripts/do-average-test.sh
+++ scripts/do-average-test.sh
@@ -7,27 +7,31 @@ then
 fi
 
 if [ "$#" -lt 3 ]; then
-    echo "Usage do-average-test.sh location-of-cmd-directory run-type output-directory"
+    echo "Usage do-average-test.sh location-of-cmd-directory run-type output-directory [quick]"
     exit 2
 fi
 
-
-GAMD_Directory=`realpath $1`
+Conventional_MD_Directory=$(realpath "$1")
 RUN_TYPE=$2
 OUTPUT_BASE=$3
 QUICK=$4
-DEBUG=$5
 
-mkdir $OUTPUT_BASE
+if [ "$QUICK" == "quick" ]; then
+  CONFIG_FILE="./tests/manual/short-debug-tests/alanine-dipeptide/$RUN_TYPE.xml"
+else
+  CONFIG_FILE="./tests/manual/full-acceptance-tests/alanine-dipeptide/$RUN_TYPE.xml"
+fi
 
-./run-test.py $RUN_TYPE $OUTPUT_BASE/1/ $QUICK $DEBUG
+mkdir "$OUTPUT_BASE"
+
+./testRunner xml "$CONFIG_FILE" -o "$OUTPUT_BASE"/1/ -a ./tests/manual/reweighting.ini
 echo ""
-./run-test.py $RUN_TYPE $OUTPUT_BASE/2/ $QUICK $DEBUG
+./testRunner xml "$CONFIG_FILE" -o "$OUTPUT_BASE"/2/ -a ./tests/manual/reweighting.ini
 echo ""
-./run-test.py $RUN_TYPE $OUTPUT_BASE/3/ $QUICK $DEBUG
+./testRunner xml "$CONFIG_FILE" -o "$OUTPUT_BASE"/3/ -a ./tests/manual/reweighting.ini
 
-COMPARISON_APP=`realpath ./tools/create-test-comparison-graphics.py`
+COMPARISON_APP=$(realpath ./tools/create-1d-analysis-graph.py)
 
-cd  $OUTPUT_BASE/; $COMPARISON_APP $GAMD_Directory 1/ 2/ 3/; mv 1D-Phi.png $RUN_TYPE-1D-Phi.png; mv 1D-Psi.png $RUN_TYPE-1D-Psi.png
+cd  "$OUTPUT_BASE/"; $COMPARISON_APP "$Conventional_MD_Directory" "Conventional MD" ./ "$RUN_TYPE"; mv 1D-Phi.png "$RUN_TYPE"-1D-Phi.png; mv 1D-Psi.png "$RUN_TYPE"-1D-Psi.png
 
 
